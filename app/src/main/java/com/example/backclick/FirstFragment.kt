c3794e6ade5a70557a89b5314b7be36eb717bd57
Dinesh <dineshsharma.m@caratlane.com>
2022-01-04 16:28:33 +0530

    with solution

--- app/src/main/java/com/example/backclick/FirstFragment.kt
+++ app/src/main/java/com/example/backclick/FirstFragment.kt
@@ -1,16 +1,19 @@
 package com.example.backclick
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.backclick.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
-    private lateinit var binding:FragmentFirstBinding
+    private lateinit var binding: FragmentFirstBinding
+    private var lastPos: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -19,14 +22,40 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_first,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
         setAdapter()
+
+
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        if (sharedPref != null) {
+            lastPos = sharedPref.getInt("lastPos", 0)
+            binding.recyclerView.scrollToPosition(lastPos)
+        }
+
+        binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                lastPos =
+                    (binding.recyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+            }
+        })
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        if (sharedPref != null) {
+            with(sharedPref.edit()) {
+                putInt("lastPos", lastPos)
+                apply()
+            }
+        }
     }
 
     private fun setAdapter() {
         val list = ArrayList<Int>()
-        for(index in 0..50){
+        for (index in 0..100) {
             list.add(index)
         }
         binding.recyclerView.adapter = RecyclerViewAdapter(list)
